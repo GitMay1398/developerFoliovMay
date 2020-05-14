b0b87ea957f02df68695990687c569832b47a3a4
MayolieC <mayolie.coulibaly@u-psud.fr>
2020-05-15 00:09:30 +0200

    supression podcast

--- src/containers/podcast/Podcast.js
+++ src/containers/podcast/Podcast.js
@@ -6,7 +6,7 @@ import { Fade } from "react-reveal";
 export default function Podcast() {
   return (
     <Fade bottom duration={1000} distance="20px">
-    <div className="main">
+    <div className="main" id="podcast">
       <div className="podcast-header">
         <h1 className="podcast-header-title">{podcastSection.title}</h1>
         <p className="subTitle podcast-header-subtitle">{podcastSection.subtitle}</p>
