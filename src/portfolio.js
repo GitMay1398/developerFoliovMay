a2b0d0c268ad0d5a54292b303e1e9d5dd4f6262e
MayolieC <mayolie.coulibaly@u-psud.fr>
2020-05-14 11:00:19 +0200

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -95,7 +95,7 @@ https://fontawesome.com/icons?d=gallery */
 // Your top 3 proficient stacks/tech experience
 
 const techStack = {
-  viewSkillBars: true, //Set it to true to show Proficiency Section
+  viewSkillBars: false, //Set it to true to show Proficiency Section
   experience: [
     {
       Stack: "Frontend/Design",  //Insert stack or technology you have experience in
