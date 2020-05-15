f62295f4b0eda7815f3f24ce3f4a27f876473cdd
MayolieC <mayolie.coulibaly@u-psud.fr>
2020-05-15 13:52:59 +0200

    supression projects

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,7 +3,7 @@ import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
 /*import StackProgress from "./skillProgress/skillProgress";*/
-import Projects from "./projects/Projects";
+/*import Projects from "./projects/Projects";*/
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
@@ -22,7 +22,6 @@ export default class Main extends Component {
         <Header />
         <Greeting />
         <Skills />
-        <Projects />
         <StartupProject />
         <Achievement />
         <Blogs />
