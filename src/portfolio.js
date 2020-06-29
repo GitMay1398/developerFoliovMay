7013b2aabdc4d54df381840d7f921984ec9e8680
MayolieC <mayolie.coulibaly@u-psud.fr>
2020-06-29 16:28:48 +0200

    Revert "test 2 mon cv"
    This reverts commit 740194f4278897857399325d45a2ee89824d4397.

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,7 +11,7 @@ const greeting = {
   title: "Bonjour, je suis Mayolie",
   subTitle: emoji("Jeune diplômée de Grenoble École de Management 🚀 Après 4 ans d'alternance dans différentes entreprises je suis à la recherche de mon premier CDI !"),
   subTitle2:"Les outils Adtech - Martech, le JavaScript et la Data n'ont aucun secret pour moi. ",
-  resumeLink: "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing"
+  resumeLink: "https://drive.google.com/file/d/1j4ct7aAxo-obGgass8u6sEDaSGF4Vet7/view?usp=sharing"
 };
 
 // Your Social Media Links
