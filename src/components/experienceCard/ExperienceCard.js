8c9c5a4626f47609c4e3a42f7e77c0b4cbd2df70
GitMay1398 <65306063+GitMay1398@users.noreply.github.com>
2021-03-21 20:11:18 +0100

    Update ExperienceCard.js

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -31,7 +31,7 @@ export default function ExperienceCard({cardInfo, isDark}) {
 
   return (
     <div className= "experience-card">
-      <div style={{background: rgb(94,96,255)}} className="experience-banner">
+      <div style={{background-color: rgb(94,96,255)}} className="experience-banner">
         <div className="experience-blurred_div"></div>
         <div className="experience-div-company">
           <h5 className="experience-text-company">{cardInfo.company}</h5>
