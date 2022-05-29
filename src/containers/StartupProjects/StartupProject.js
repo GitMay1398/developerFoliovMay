a3d28f322c4e78d0282407498d2890d9795e23bb
GitMay1398 <65306063+GitMay1398@users.noreply.github.com>
2022-05-29 19:12:09 +0200

    project change

--- src/containers/StartupProjects/StartupProject.js
+++ src/containers/StartupProjects/StartupProject.js
@@ -12,23 +12,35 @@ export default function StartupProject() {
   return (
     <Fade bottom duration={1000} distance="20px">
     <div className="main" id="projects">
-      <div>
-        <h1 className="skills-heading">{bigProjects.title}</h1>
-        <p className="subTitle project-subtitle">{bigProjects.subtitle}</p>
-        <div className="startup-projects-main">
-          <div className="startup-project-text">
-            {bigProjects.projects.map(project => {
-              return (
-                <div className="saaya-health-div" >
-                  <img alt={project.imageAlt} src={project.image}></img>
-                  <div className="descrProjets"> {project.projectDescription}</div>
-                </div>
-              );
-            })}
-          </div>
+    <div>
+    <h1 className="skills-heading">{bigProjects.title}</h1>
+    <p className="subTitle project-subtitle">{bigProjects.subtitle}</p>
+    <div className="startup-projects-main">
+    <div className="startup-project-text">
+    <section class="tiles">
+    {bigProjects.projects.map(project => {
+      return (
+        
+        <article class="style1">
+        <span class="image">
+        <img src={project.image} alt={project.imageAlt}/>
+        </span>
+        <a href="" target="_blank">
+        <h2>Gate 13</h2>
+        <div class="content">
+        <p>{project.projectDescription}</p>
         </div>
-      </div>
+        </a>
+        </article>
+        
+        );
+    })}
+    </section>
     </div>
+    </div>
+    </div>
+
     </Fade>
-  );
-}
+    
+    );
+  }
