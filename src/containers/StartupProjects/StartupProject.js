cf1aec2eaf490ed78e8f0f658a6bc8231336249b
GitMay1398 <65306063+GitMay1398@users.noreply.github.com>
2022-05-29 20:15:50 +0200

    fix div

--- src/containers/StartupProjects/StartupProject.js
+++ src/containers/StartupProjects/StartupProject.js
@@ -11,33 +11,31 @@ export default function StartupProject() {
 
   return (
     <Fade bottom duration={1000} distance="20px">
-    <div className="main" id="projects">
-    <div>
-    <h1 className="skills-heading">{bigProjects.title}</h1>
-    <p className="subTitle project-subtitle">{bigProjects.subtitle}</p>
-    <div className="startup-projects-main">
-    <div className="startup-project-text">
-    <section class="tiles">
-    {bigProjects.projects.map(project => {
-      return (
+      <div className="main" id="projects">
+          <h1 className="skills-heading">{bigProjects.title}</h1>
+          <p className="subTitle project-subtitle">{bigProjects.subtitle}</p>
+          <div className="startup-projects-main">
+          <div className="startup-project-text">
+          <section class="tiles">
+              {bigProjects.projects.map(project => {
+              return (
         
-        <article class="style1">
-        <span class="image">
-        <img src={project.image} alt={project.imageAlt}/>
-        </span>
-        <a href="" target="_blank">
-        <h2>Gate 13</h2>
-        <div class="content">
-        <p>{project.projectDescription}</p>
-        </div>
-        </a>
-        </article>
-        
-        );
-    })}
-    </section>
-    </div>
-    </div>
+                <article class="style1">
+                  <span class="image">
+                  <img src={project.image} alt={project.imageAlt}/>
+                  </span>
+                  <a href="" target="_blank">
+                      <h2>Gate 13</h2>
+                      <div class="content">
+                          <p>{project.projectDescription}</p>
+                      </div>
+                  </a>
+                </article>
+              );
+              })}
+          </section>
+          </div>
+         </div>
     </div>
 
     </Fade>
